a7c2281cbba50bfb589bb0597b6ca3968956b999
David Núñez <david@nucypher.com>
2018-07-03 22:47:25 +0200

    Deploy and link FastSecp256k1.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
+const FastSecp256k1 = artifacts.require('FastSecp256k1.sol');
 var Verifier = artifacts.require("./Verifier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Verifier, {gas: 5000000});
+	deployer.deploy(FastSecp256k1).then(() => {
+  		deployer.deploy(Verifier, {gas: 5000000});
+  	});
+  	deployer.link(FastSecp256k1, Verifier);
 };
