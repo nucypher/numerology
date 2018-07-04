b2d61deb5ce03f5a0d9cdea9545305c1e1e8e1d3
David Núñez <david@nucypher.com>
2018-07-05 01:31:35 +0200

    Deploying verifier to get gas estimation

--- contracts/verifier.sol
+++ contracts/verifier.sol
@@ -4,6 +4,21 @@ import "./FastSecp256k1.sol";
 
 contract Verifier {
 
+  function testSim_Mul() public returns (bool) {
+    // Verifier.deployed().then(function(instance) { return instance.testSim_Mul.estimateGas(); })
+
+    int256[4] memory k_l = [int256(-89243190524605339210527649141408088119), int256(-53877858828609620138203152946894934485), int256(-185204247857117235934281322466442848518), int256(-7585701889390054782280085152653861472)];
+
+    uint256[4] memory P_Q = [0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798, 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8, 0xc6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5, 0x1ae168fea63dc339a3c58419466ceaeef7f632653266d0e1236431a950cfe52a];
+    
+    uint256[3] memory kP_lQ = FastSecp256k1._sim_mul(k_l, P_Q);
+
+    uint256[3] memory expected = [0x7635e27fba8e1f779dcfdde1b1eacbe0571fbe39ecf6056d29ba4bd3ef5e22f2, 0x197888e5cec769ac2f1eb65dbcbc0e49c00a8cdf01f8030d8286b68c1933fb18, 1];
+
+    return FastSecp256k1.eq_jacobian(kP_lQ, expected);
+
+  }
+
   function jarl() public returns (bool){
 /*
 e = '03a6eed4bb507beba20f8f90e7092d700869825820c6fc11d76949fdd31c3c9e81'
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,8 @@
 const FastSecp256k1 = artifacts.require('FastSecp256k1.sol');
-var Verifier = artifacts.require("./Verifier.sol");
+var Verifier = artifacts.require("Verifier.sol");
 
 module.exports = function(deployer) {
 	deployer.deploy(FastSecp256k1);
- // deployer.deploy(FastSecp256k1).then(() => {
- //  		deployer.deploy(Verifier, {gas: 5000000});
- //  	});
- //  	deployer.link(FastSecp256k1, Verifier);
+  	deployer.link(FastSecp256k1, Verifier);
+  	deployer.deploy(Verifier);
 };
