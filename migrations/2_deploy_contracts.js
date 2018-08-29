bf4040eecb6c2a88f8b07bd0d781d65063b4ccb9
David Núñez <david@nucypher.com>
2018-08-29 14:22:24 +0200

    Rename Numerology source file and update references everywhere else

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-const FastSecp256k1 = artifacts.require('FastSecp256k1.sol');
+const Numerology = artifacts.require('Numerology.sol');
 var Verifier = artifacts.require("Verifier.sol");
 
 module.exports = function(deployer) {
-	deployer.deploy(FastSecp256k1);
-  	deployer.link(FastSecp256k1, Verifier);
+	deployer.deploy(Numerology);
+  	deployer.link(Numerology, Verifier);
   	deployer.deploy(Verifier);
 };
