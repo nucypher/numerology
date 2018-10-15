5dec10e4d42fe18b5f08fd57a0b326e0a78725ca
David Núñez <david@nucypher.com>
2018-10-16 00:01:20 +0200

    Reencryption correctness check PoC using the ecrecover hack (<44k gas!!)

--- contracts/verifier.sol
+++ contracts/verifier.sol
@@ -38,5 +38,32 @@ contract Verifier {
 
   }
 
+  function test_proof_verification_hack() public view returns (bool) {
+    // Verifier.deployed().then(function(inst) { return inst.test_proof_verification_hack.estimateGas(); })
+
+    uint256[2] memory k_l = [0x1787f38d854231dfec2b27a0f621414d10bfa95970b3e576aed29e1e8287e51e,
+                             0x60d4afe7ba1967e921f22bb9fbd277f10cfe04810451d7c549ae9099c4a8b677];
+
+    uint256[4] memory P_Q = [0xa6ecb3f599964fe04c72e486a8f90172493c21f4185f1ab9a7fe05659480c548,
+                             0xdf67fd3f4255826c234a5262adc70e14a6d42f13ee55b65e885e666e1dd5d3f5,
+                             0x3f75f99c8df97f477b700407dd7a956cee2e4915f2df4fa9b11c403935c05dc5,
+                             0x497495d2e486fd4cff4929265eac91711014b9d6ad1c2220d709e20d466627fd];
+    
+    
+    uint256[6] memory expected = [0xaddcb45773b26a2f8ac2143627d54f47a12aab533dc1b41b4e791985e9eca496, // kP_x
+                                  0x72da5adb3a30a2cf147d309b0cf58c76b322c82a5edae164e13dbeed6429c41d, // kP_y
+                                  0xf07716879380e987f8b5551a1d989068d0003061088a869a33ceb9848771c6fd, // lQ_x
+                                  0x2447ed4564b75b0f9ff84013aaa37c2ab67a2c621b0edc91a06895f19a93aebb, // lQ_y
+                                  0x9ca8f6ff6a2eb6f62787f70b9f7c4939d1a3890ec87343e4f6716f9f6867eb86, // Rx
+                                  0x290c40f22995dc8b956d2c63ec060d332d082124d638ed618891171db8bc206f // Ry
+                                  ];
+
+    bool sum_is_correct = Numerology.eq_jacobian(Numerology.addJac([expected[0], expected[1], 1], [expected[2], expected[3], 1]), [expected[4], expected[5], 1]);
+    bool kP_is_correct = Numerology.ecmulVerify(P_Q[0], P_Q[1], k_l[0], expected[0], expected[1]);
+    bool lQ_is_correct = Numerology.ecmulVerify(P_Q[2], P_Q[3], k_l[1], expected[2], expected[3]);
+
+    return sum_is_correct && kP_is_correct && lQ_is_correct;
+
+  }
 }
 
